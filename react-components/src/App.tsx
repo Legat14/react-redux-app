e2e37356fdda8c8cb6069872bf46c7bcb790b83e
Legat <zia-d@mail.ru>
2022-09-29 17:10:09 +0400

    feat: add base routing

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import MainPage from 'pages/Main-page';
+import AboutUsPage from 'pages/About-page';
+import DoesntExist from 'pages/404';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-page-header">
+        <h1>React components</h1>
       </header>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="*" element={<DoesntExist />} />
+      </Routes>
     </div>
   );
 }
