e2c41a8b0c1e9da70f7d032496a1472deafc72aa
Legat <zia-d@mail.ru>
2022-09-29 17:42:29 +0400

    feat: add layout

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import MainPage from 'pages/Main-page';
-import AboutUsPage from 'pages/About-page';
-import DoesntExist from 'pages/404';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from 'components/pages/Main-page';
+import AboutUsPage from 'components/pages/About-page';
+import DoesntExist from 'components/pages/404';
+import Layout from 'components/Layout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="main-page-header">
-        <h1>React components</h1>
-        <nav>
-          <Link to="/">Main page</Link>
-          <Link to="/about">About us</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        <Route path="*" element={<DoesntExist />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="*" element={<DoesntExist />} />
+        </Route>
       </Routes>
     </div>
   );
