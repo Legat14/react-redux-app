a4ffa09f7dd4e3375cea3a1761b7c489c01d2f0e
Legat <zia-d@mail.ru>
2022-10-01 21:35:24 +0400

    feat: add input and button tests

--- react-components/src/App.test.tsx
+++ react-components/src/App.test.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders React components', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByText(/React components/i);
-  expect(linkElement).toBeInTheDocument();
+describe ('App', () => {
+  it('renders React components text in App component', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    screen.debug();
+    const reactCompText = screen.getByText(/React components/i);
+    expect(reactCompText).toBeInTheDocument();
+  });
+  it('renders search input in App component', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    screen.debug();
+    const searchInput = screen.getByRole('searchbox');
+    expect(searchInput).toBeInTheDocument();
+  });
+  it('renders search button in App component', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    screen.debug();
+    const searchBtn = screen.getByRole('button');
+    expect(searchBtn).toBeInTheDocument();
+  });
 });
