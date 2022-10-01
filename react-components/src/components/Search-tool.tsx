84142345b776434aa4bee636f727ec5e55affe24
Legat <zia-d@mail.ru>
2022-10-01 11:22:07 +0400

    feat: add saving and loading search input from localStorage

--- react-components/src/components/Search-tool.tsx
+++ react-components/src/components/Search-tool.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { LegacyRef } from 'react';
 
-function SearchTool() {
-  return (
-    <form className='search-tool'>
-      <input type='search' placeholder='Enter request here'></input>
-      <button type='button'>Search</button>
-    </form>
-  );
+class SearchTool extends React.Component {
+  constructor() {
+    super({}); // TODO: Из-за этого TS выдает ошибку. Нужно разобраться с типизацией.
+    this.getValue = this.getValue.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+
+  getValue(): string | null {
+    const input = this.inputRef.current;
+    let searchInput = null;
+    if (input) {
+      console.log(input.value);
+      searchInput = input.value
+    }
+    return searchInput;
+  }
+
+  setValue(searchValue: string): void{
+    const input = this.inputRef.current;
+    if (input) {
+      input.value = searchValue;
+    }
+  }
+
+  componentDidMount(): void {
+    const searchInput = localStorage.getItem('searchInput');
+    console.log('Did mount: ', searchInput);
+    if (searchInput || searchInput === '') {
+      this.setValue(searchInput);
+    }
+  }
+
+  componentWillUnmount(): void {
+    const searchInput = this.getValue();
+    console.log('Will unmount: ', searchInput);
+    if (searchInput || searchInput === '') {
+      localStorage.setItem('searchInput', searchInput);
+    }
+  }
+
+  search(): void { // Это метод-заглушка. Познее нужно его расширить до полноценного поиска.
+    const input = this.inputRef.current
+    if (input) {
+      console.log('Search: ', input.value);
+    }
+  }
+
+  render(): React.ReactNode {
+    return (
+      <form className='search-tool'>
+        <input type='search' placeholder='Enter request here' ref={this.inputRef as LegacyRef<HTMLInputElement>}></input>
+        <button type='button' onClick={this.search}>Search</button>
+      </form>
+    );
+  }
 }
 
 export default SearchTool;
