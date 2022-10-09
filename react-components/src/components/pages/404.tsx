9f2ab47b11bc772457b0b8266971e9bc72869db9
Legat <zia-d@mail.ru>
2022-10-09 14:35:52 +0400

    feat: add styles to 404 page

--- react-components/src/components/pages/404.tsx
+++ react-components/src/components/pages/404.tsx
@@ -4,7 +4,7 @@ function DoesntExist(): JSX.Element {
   return (
     <section className="nothing-found-page__section">
       <h2>404</h2>
-      <h3>This page does not exist</h3>
+      <p>This page does not exist</p>
     </section>
   );
 }
