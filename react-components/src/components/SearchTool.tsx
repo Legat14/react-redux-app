e29be16763b765f3c3ceb1e7d9158d59824fe30c
Ivan Zotov <zia14leg@gmail.com>
2022-10-12 18:14:01 +0400

    refactor: rewrite input method to controlled input

--- react-components/src/components/SearchTool.tsx
+++ react-components/src/components/SearchTool.tsx
@@ -1,10 +1,14 @@
-import React, { LegacyRef } from 'react';
+import React from 'react';
 
-class SearchTool extends React.Component {
+class SearchTool extends React.Component<{}, {request: string}> {
   constructor(props: {}) {
     super(props);
     this.getValue = this.getValue.bind(this);
     this.search = this.search.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      request: '',
+    }
   }
 
   inputRef: React.RefObject<HTMLInputElement> = React.createRef();
@@ -41,9 +45,15 @@ class SearchTool extends React.Component {
 
   search(): void {
     // Это метод-заглушка. Познее нужно его расширить до полноценного поиска.
-    const input = this.inputRef.current;
-    if (input) {
-      console.log('Search: ', input.value);
+    console.log('Search: ', this.state.request);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (event.target) {
+      const value = event.target.value;
+      this.setState({
+        request: value,
+      });
     }
   }
 
@@ -54,7 +64,9 @@ class SearchTool extends React.Component {
           className="input-search"
           type="search"
           placeholder="Enter request here"
-          ref={this.inputRef as LegacyRef<HTMLInputElement>}
+          value={this.state.request}
+          onChange = {this.handleChange}
+          ref={this.inputRef as React.RefObject<HTMLInputElement>}
         ></input>
         <button className="search-btn" type="button" onClick={this.search}>
           Search
