1f214ecda49fa6eb84196ff6a34e981e430e5025
Ivan Zotov <zia14leg@gmail.com>
2022-10-12 23:02:15 +0400

    feat: make url generator

--- react-components/src/components/SearchTool.tsx
+++ react-components/src/components/SearchTool.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { FormEventHandler, ReactEventHandler } from 'react';
 
 class SearchTool extends React.Component<{}, {request: string}> {
+  requestEndpoint: string;
+  requestMethod: string;
+  apiKey: string;
+  format: string;
+
   constructor(props: {}) {
     super(props);
+    this.requestEndpoint = 'https://www.flickr.com/services/rest/';
+    this.requestMethod = 'flickr.photos.search';
+    this.apiKey = '55f3f75e00ac276da38071522de9c95a'; // TODO: Заменить хардкод на переменную энвайромента
+    this.format = 'json&nojsoncallback=1';
     this.getValue = this.getValue.bind(this);
     this.search = this.search.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -43,9 +52,17 @@ class SearchTool extends React.Component<{}, {request: string}> {
     }
   }
 
-  search(): void {
-    // Это метод-заглушка. Познее нужно его расширить до полноценного поиска.
+  search(event: React.FormEvent<HTMLFormElement>): void {
+    (event ).preventDefault();
     console.log('Search: ', this.state.request);
+    const request = this.state.request;
+    const requestArr = request.split(' ');
+    const trimmedRequest = requestArr.map((element): string =>{
+      return element.trim();
+    });
+    const processedRequest = trimmedRequest.join('+');
+    const requestUrl = `${this.requestEndpoint}?method=${this.requestMethod}&api_key=${this.apiKey}&text=${processedRequest}&format=${this.format}`
+    console.log(requestUrl);
   }
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -59,7 +76,7 @@ class SearchTool extends React.Component<{}, {request: string}> {
 
   render(): JSX.Element {
     return (
-      <form className="search-tool">
+      <form className="search-tool" onSubmit={this.search}>
         <input
           className="input-search"
           type="search"
@@ -68,7 +85,7 @@ class SearchTool extends React.Component<{}, {request: string}> {
           onChange = {this.handleChange}
           ref={this.inputRef as React.RefObject<HTMLInputElement>}
         ></input>
-        <button className="search-btn" type="button" onClick={this.search}>
+        <button className="search-btn" type="submit">
           Search
         </button>
       </form>
