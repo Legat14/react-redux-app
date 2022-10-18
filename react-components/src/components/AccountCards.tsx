2382dab4dad8731f111f9485d624062eca348628
Ivan Zotov <zia14leg@gmail.com>
2022-10-18 17:47:49 +0400

    refactor: change AccountCards back to functional component

--- react-components/src/components/AccountCards.tsx
+++ react-components/src/components/AccountCards.tsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { IAccountCard } from 'types';
 import AccountCard from './AccountCard';
 
-class AccountCards extends React.Component<{ cardData: IAccountCard[] }> {
-  render(): JSX.Element {
-    const cardsArr = this.props.cardData.map((element): JSX.Element => {
-      return (
-        <AccountCard
-          key={element.key}
-          name={element.name}
-          birthDate={element.birthDate}
-          gender={element.gender}
-          avatar={element.avatar}
-          avatarUrl={element.avatarUrl}
-          country={element.country}
-          devices={element.devices}
-        />
-      );
-    });
-
+function AccountCards(props: {cardData: IAccountCard[]}): JSX.Element {
+  const cardsArr = props.cardData.map((element): JSX.Element => {
     return (
-      <div className="account-cards__div">
-        <h3>Accounts registered: {this.props.cardData.length}</h3>
-        {cardsArr}
-      </div>
+      <AccountCard
+        key={element.key}
+        name={element.name}
+        birthDate={element.birthDate}
+        gender={element.gender}
+        avatar={element.avatar}
+        avatarUrl={element.avatarUrl}
+        country={element.country}
+        devices={element.devices}
+      />
     );
-  }
+  });
+  return (
+    <div className="account-cards__div">
+      <h3>Accounts registered: {props.cardData.length}</h3>
+      {cardsArr}
+    </div>
+  );
 }
 
 export default AccountCards;
