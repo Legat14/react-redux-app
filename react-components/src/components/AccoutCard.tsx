731d2e8367fd43a9e57c17917e8fb34265e816c0
Ivan Zotov <zia14leg@gmail.com>
2022-10-18 17:29:18 +0400

    refactor: change AccountCard back to functional component

--- react-components/src/components/AccoutCard.tsx
+++ react-components/src/components/AccoutCard.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
 import { IAccountCard } from 'types';
 
-class AccountCard extends React.Component<IAccountCard> {
-  constructor(props: IAccountCard) {
-    super(props);
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className="account-card">
-        <h4>{this.props.name}</h4>
-        <div className="account-card__avatar-div">
-          <img className="account-card__avatar-img" src={this.props.avatarUrl} alt="avatar" />
-        </div>
-        <div className="account-card__div">
-          <p>
-            <strong>Birth date:</strong>
-            <span>{this.props.birthDate}</span>
-          </p>
-          <p>
-            <strong>Gender:</strong>
-            <span>{this.props.gender}</span>
-          </p>
-          <p>
-            <strong>Country:</strong>
-            <span>{this.props.country}</span>
-          </p>
-          <p>
-            <strong>Gaming devices:</strong>
-            <span>{this.props.devices}</span>
-          </p>
-        </div>
+function AccountCard(props: IAccountCard): JSX.Element {
+  return (
+    <div className="account-card">
+      <h4>{props.name}</h4>
+      <div className="account-card__avatar-div">
+        <img className="account-card__avatar-img" src={props.avatarUrl} alt="avatar" />
       </div>
-    );
-  }
+      <div className="account-card__div">
+        <p>
+          <strong>Birth date:</strong>
+          <span>{props.birthDate}</span>
+        </p>
+        <p>
+          <strong>Gender:</strong>
+          <span>{props.gender}</span>
+        </p>
+        <p>
+          <strong>Country:</strong>
+          <span>{props.country}</span>
+        </p>
+        <p>
+          <strong>Gaming devices:</strong>
+          <span>{props.devices}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AccountCard;
