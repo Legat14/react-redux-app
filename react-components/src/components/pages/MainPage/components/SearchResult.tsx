712ac1754b8a1b0f9b940e6a2a3a002eaaf365d5
Ivan Zotov <zia14leg@gmail.com>
2022-10-18 20:27:13 +0400

    refactor: move GameCard to MainPage folder

--- react-components/src/components/pages/MainPage/components/SearchResult.tsx
+++ react-components/src/components/pages/MainPage/components/SearchResult.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GameCard from '../../../GameCard';
+import GameCard from './GameCard';
 import { mockData } from '../../../../model/mockData';
 
 class SearchResult extends React.Component<{
