9f643250927c65baf0ef19bbd14f75ed2e214b91
Ivan Zotov <zia14leg@gmail.com>
2022-10-21 21:58:08 +0400

    refactor: change ModalWindowOverlay back to function

--- react-components/src/components/pages/MainPage/MainPage.tsx
+++ react-components/src/components/pages/MainPage/MainPage.tsx
@@ -10,10 +10,10 @@ class MainPage extends React.Component<
   {
     response: IResponse | {};
     modalContent: IModalContent | {};
+    isOpened: boolean;
     isLoaded: boolean;
   }
 > {
-  overlay: React.RefObject<ModalWindowOverlay>;
   modal: React.RefObject<ModalWindow>;
 
   constructor(props: {}) {
@@ -23,11 +23,11 @@ class MainPage extends React.Component<
     this.openModalWindow = this.openModalWindow.bind(this);
     this.closeModalWindow = this.closeModalWindow.bind(this);
     this.setIsLoaded = this.setIsLoaded.bind(this);
-    this.overlay = React.createRef();
     this.modal = React.createRef();
     this.state = {
       response: {},
       modalContent: {},
+      isOpened: false,
       isLoaded: true,
     };
   }
@@ -69,18 +69,18 @@ class MainPage extends React.Component<
   };
 
   openModalWindow(): void {
-    if (this.overlay.current) {
-      this.overlay.current.showOverlay();
-    }
+    console.log('open');
+    this.setState({isOpened: true});
+    console.log(this.state);
     if (this.modal.current) {
       this.modal.current.showModel();
     }
   }
-
+  
   closeModalWindow(): void {
-    if (this.overlay.current) {
-      this.overlay.current.hideOverlay();
-    }
+    console.log('close');
+    this.setState({isOpened: false});
+    console.log(this.state);
     if (this.modal.current) {
       this.modal.current.hideModel();
     }
@@ -89,7 +89,7 @@ class MainPage extends React.Component<
   render(): JSX.Element {
     return (
       <section className="main-page__section" onClick={this.closeModalWindow}>
-        <ModalWindowOverlay ref={this.overlay} />
+        <ModalWindowOverlay isOpened={this.state.isOpened} />
         <ModalWindow
           ref={this.modal}
           modalContent={
@@ -129,3 +129,128 @@ class MainPage extends React.Component<
 }
 
 export default MainPage;
+
+// class MainPage extends React.Component<
+//   {},
+//   {
+//     response: IResponse | {};
+//     modalContent: IModalContent | {};
+//     isLoaded: boolean;
+//   }
+// > {
+//   overlay: React.RefObject<{hideOverlay: () => void, showOverlay: () => {}}>;
+//   modal: React.RefObject<ModalWindow>;
+
+//   constructor(props: {}) {
+//     super(props);
+//     this.getPhotos = this.getPhotos.bind(this);
+//     this.getModalContent = this.getModalContent.bind(this);
+//     this.openModalWindow = this.openModalWindow.bind(this);
+//     this.closeModalWindow = this.closeModalWindow.bind(this);
+//     this.setIsLoaded = this.setIsLoaded.bind(this);
+//     this.overlay = React.createRef();
+//     this.modal = React.createRef();
+//     this.state = {
+//       response: {},
+//       modalContent: {},
+//       isLoaded: true,
+//     };
+//   }
+
+//   setIsLoaded(value: boolean) {
+//     this.setState((prev) => {
+//       return {
+//         response: {},
+//         modalContent: prev.modalContent,
+//         isLoaded: value,
+//       };
+//     });
+//   }
+
+//   getPhotos = (response: IResponse): void => {
+//     this.setState((prev) => {
+//       return {
+//         response: response,
+//         modalContent: prev.modalContent,
+//         isLoaded: true,
+//       };
+//     });
+//   };
+
+//   getModalContent = (photo: IPhoto, src: string): void => {
+//     this.setState((prev) => {
+//       return {
+//         response: prev.response,
+//         modalContent: {
+//           src: src,
+//           id: photo.id,
+//           owner: photo.owner,
+//           server: photo.server,
+//           title: photo.title,
+//         },
+//       };
+//     });
+//     this.openModalWindow();
+//   };
+
+//   openModalWindow(): void {
+//     if (this.overlay.current) {
+//       this.overlay.current.showOverlay();
+//     }
+//     if (this.modal.current) {
+//       this.modal.current.showModel();
+//     }
+//   }
+
+//   closeModalWindow(): void {
+//     if (this.overlay.current) {
+//       this.overlay.current.hideOverlay();
+//     }
+//     if (this.modal.current) {
+//       this.modal.current.hideModel();
+//     }
+//   }
+
+//   render(): JSX.Element {
+//     return (
+//       <section className="main-page__section" onClick={this.closeModalWindow}>
+//         <ModalWindowOverlay ref={this.overlay} />
+//         <ModalWindow
+//           ref={this.modal}
+//           modalContent={
+//             (
+//               this.state as {
+//                 response: IResponse | {};
+//                 modalContent: IModalContent | {};
+//               }
+//             ).modalContent
+//           }
+//           closeModalWindow={this.closeModalWindow}
+//         />
+//         <div className="main-page__bar">
+//           <h2>Main page</h2>
+//           <SearchTool getPhotos={this.getPhotos} setIsLoaded={this.setIsLoaded} />
+//         </div>
+//         {this.state.isLoaded ? (
+//           <SearchResult
+//             response={
+//               (
+//                 this.state as {
+//                   response: IResponse | {};
+//                   modalContent: IModalContent | {};
+//                 }
+//               ).response
+//             }
+//             getModalContent={this.getModalContent}
+//           />
+//         ) : (
+//           <div className="main-page__loading-screen">
+//             <img src="./assets/gif/loading-screen.gif" alt="loading..." />
+//           </div>
+//         )}
+//       </section>
+//     );
+//   }
+// }
+
+// export default MainPage;
--- react-components/src/components/pages/MainPage/components/ModalWindowOverlay.tsx
+++ react-components/src/components/pages/MainPage/components/ModalWindowOverlay.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 
-class ModalWindowOverlay extends React.Component<{}, { overlayClass: string }> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      overlayClass: 'modal-window__overlay modal-window__hidden',
-    };
-    this.showOverlay = this.showOverlay.bind(this);
-    this.hideOverlay = this.hideOverlay.bind(this);
+function ModalWindowOverlay(props: {isOpened: boolean}): JSX.Element {
+  let overlayClass = 'modal-window__overlay modal-window__hidden';
+
+  if (props.isOpened) {
+    overlayClass = 'modal-window__overlay';
   }
 
-  showOverlay() {
-    this.setState({
-      overlayClass: 'modal-window__overlay',
-    });
-  }
-
-  hideOverlay() {
-    this.setState({
-      overlayClass: 'modal-window__overlay modal-window__hidden',
-    });
-  }
-
-  render(): JSX.Element {
-    return <div className={this.state.overlayClass}></div>;
-  }
+  return <div className={overlayClass}></div>;
 }
 
 export default ModalWindowOverlay;
