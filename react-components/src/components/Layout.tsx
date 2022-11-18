bc575a7c83ccfb1fd5ecca78258e7f2c6f810b7c
Ivan Zotov <zia14leg@gmail.com>
2022-11-18 09:55:46 +0400

    refactor: move Providerto index.tsx

--- react-components/src/components/Layout.tsx
+++ react-components/src/components/Layout.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import store from '../store/store';
-import { Provider } from 'react-redux';
 
 function Layout(): JSX.Element {
 
   return (
-    <Provider store={store}>
-      <div className="app-conteiner">
-        <Header />
-        <Outlet />
-      </div>
-    </Provider>
+    <div className="app-conteiner">
+      <Header />
+      <Outlet />
+    </div>
   );
 }
 
--- react-components/src/index.tsx
+++ react-components/src/index.tsx
@@ -5,12 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'App';
 import addBeforeUnloadEvent from 'controller/addBeforeUnloadEvent';
+import { Provider } from 'react-redux';
+import store from 'store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
